51b5fe2ba21d3b10708f7ff0752fbf6b83ae3274
Ashutosh Chaudhary <216.ashutosh@gmail.com>
2025-02-17 18:53:34 +0530

    feat(delivery): add owner_id column
    feat(main): add legacy mode
    
    legacy mode uses legacy subscriptions
    legacy subscriptions are specific to my usecase
    and will be removed eventually

--- migrations/mysql/000002_add_event_delivery_tbl.up.sql
+++ migrations/mysql/000002_add_event_delivery_tbl.up.sql
@@ -2,17 +2,16 @@ CREATE TABLE IF NOT EXISTS hookrelay.event_delivery (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     event_type VARCHAR(255) NOT NULL,
     payload TEXT,
+    owner_id VARCHAR(255) NOT NULL,
     subscription_id VARCHAR(255) NOT NULL,
     status_code INT,
     error TEXT,
     start_at TIMESTAMP NULL DEFAULT NULL,
     complete_at TIMESTAMP NULL DEFAULT NULL,
-
-    -- Foreign key constraints
-    CONSTRAINT fk_subscription FOREIGN KEY (subscription_id) REFERENCES hookrelay.subscription(id) ON DELETE NO ACTION
 );
 
 -- Optional: Add indexes for faster queries
+CREATE INDEX idx_eventdelivery_owner_id ON hookrelay.event_delivery (owner_id);
 CREATE INDEX idx_eventdelivery_subscription_id ON hookrelay.event_delivery (subscription_id);
 CREATE INDEX idx_eventdelivery_event_type ON hookrelay.event_delivery (event_type);
 CREATE INDEX idx_eventdelivery_start_at ON hookrelay.event_delivery (start_at);
